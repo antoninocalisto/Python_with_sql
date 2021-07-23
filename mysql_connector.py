b0f625c37d7761622015439b868ffa7b076ed235
antoninocalisto <antuninosantos@gmail.com>
2021-07-23 15:42:12 -0300

    Adicionando consultas no sql em forma de loop e modelo de select da API do banco

--- mysql_connector.py
+++ mysql_connector.py
@@ -10,7 +10,9 @@ configuracao = {
 };
 
 try:
+    #criando a conexão
     conexao = mysql.connector.connect(**configuracao);
+    cursor = conexao.cursor();
 
 except mysql.connector.Error as erro:
         if erro.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -20,4 +22,44 @@ except mysql.connector.Error as erro:
         else:
             print("Erro");
 else:
+    
+    while(True):
+        #comando
+        inserir_cliente = ("insert into cliente (nome, telefone, observacao, cidade, estado, tipo_pessoa)" 
+        "values (%(nome_contato)s, %(telefone)s, %(observacao)s, %(cidade)s, %(estado)s, %(tipo_pessoa)s)");
+        
+        #Consultas
+        
+        '''
+        ##Select  
+        consulta = ("select nome, telefone, celular from contatos where nome like 'M%'");
+        cursor.execute(consulta);
+
+        for (nome, telefone, celular) in cursor:
+            print(f"Nome: {nome}, telefone: {telefone}, celular: {celular}");
+        ## FIM SELECT #############################
+        '''
+
+        #Dados do cliente:
+        _nome = input("\nNome do cliente: ")
+        _telefone = input("\Telefone do cliente: ")
+        _observacao = input("\nOnde entregar: ")
+        _cidade = input("\nCidade do cliente: ")
+        _estado = input("\nEstado: ")
+        _tipo_pessoa = input("\nFisica ou Juridica? ")
+        
+
+        #valores
+        cliente = {
+            'nome_contato' : _nome,
+            'telefone' : _telefone,
+            'observacao' : _observacao,
+            'cidade' : _cidade,
+            'estado' : _estado,
+            'tipo_pessoa' : _tipo_pessoa
+        };
+
+        cursor.execute(inserir_cliente, cliente);
+        conexao.commit();
+
     conexao.close();
